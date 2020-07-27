9472e6919fa86d6d02bd2a7bfc882257243e0028
郑佳坛 <13060590076@163.com>
2020-07-27 14:54:49 +0800

    修复 XSS 注入漏洞

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Observers\UserObserver;
+use App\Observers\ReplyObserver;
+use App\Observers\TopicObserver;
+use App\Models\Reply;
+use App\Models\Topic;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
+        // Reply::observe(ReplyObserver::class);
+        Topic::observe(TopicObserver::class);
     }
 }

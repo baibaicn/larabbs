e6745819657212d713989629ac76f4b57093ac33
郑佳坛 <13060590076@163.com>
2020-07-31 17:10:21 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
         </p>
 
 
-        <p class="float-right"><a href="#">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>

2fbbb2b0b34a2245b49bf7d3ef365649fadea562
郑佳坛 <13060590076@163.com>
2020-08-26 11:59:31 +0800

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,17 @@ Route::prefix('v1')
 	
 		Route::middleware('throttle:' . config('api.rate_limits.sign'))
 			->group(function () {
+				// 图片验证码
+				Route::post('captchas', 'CaptchasController@store')
+					->name('captchas.store');
+					
 				// 短信验证码					
 				Route::post('verificationCodes', 'VerificationCodesController@store')
-				->name('verificationCodes.store');    
+					->name('verificationCodes.store');
+				
 				// 用户注册
 				Route::post('users', 'UsersController@store')
-				->name('users.store');
+					->name('users.store');
 	
 			});
 

81988eb3b0acec2589c9d1960687e41906dacdb0
郑佳坛 <13060590076@163.com>
2020-08-28 17:44:12 +0800

    发布话题

--- routes/api.php
+++ routes/api.php
@@ -41,8 +41,10 @@ Route::prefix('v1')
 			->group(function () {
 				// 游客可以访问的接口
 
-
-
+				// 话题列表，详情
+				Route::resource('topics', 'TopicsController')->only([
+					'index', 'show'
+				]);
 				// 某个用户的详情
 				Route::get('users/{user}', 'UsersController@show')
 					->name('users.show');
@@ -50,6 +52,8 @@ Route::prefix('v1')
 				Route::get('categories','CategoriesController@index')
 					->name('categories.index');
 
+
+					
 				// 登录后可以访问的接口
 				Route::middleware('auth:api')->group(function() {
 					// 当前登录用户信息
@@ -61,6 +65,11 @@ Route::prefix('v1')
 					// 上传图片
 					Route::post('images', 'ImagesController@store')
 						->name('images.store');
+					// 发布话题
+					Route::resource('topics', 'TopicsController')->only([
+						'store', 'update', 'destroy'
+					]);
+
 				});
 					
 			});
